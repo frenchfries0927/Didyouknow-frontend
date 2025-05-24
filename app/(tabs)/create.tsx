cc25a69a846ce626db5e4675e8ae042174597545
sangyeop123 <saddleonwind@gmail.com>
2025-05-24 19:48:48 +0900

    image solved

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 
 // 이 파일은 단지 탭 네비게이션에서 + 버튼을 위한 빈 화면입니다.
 // 실제 기능은 TabLayout에서 버튼을 클릭했을 때 create-post 화면으로 이동하도록 구현되어 있습니다.
-const API_URL = 'http://172.30.1.88:8080';  // 개발 컴퓨터의 IP 주소
+const API_URL = 'http://localhost:8080';  // 로컬 개발 서버
 
 export default function CreatePlaceholder() {
   return <View />;
